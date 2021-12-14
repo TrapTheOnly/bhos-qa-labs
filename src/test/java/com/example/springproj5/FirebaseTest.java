3a5d008fdfd730a2e4a07529926ca84301c32552
TrapTheOnly <ismail.eyyub@gmail.com>
2021-12-14 13:33:09 +0400

    Initial Release

--- src/test/java/com/example/springproj5/FirebaseTest.java
+++ src/test/java/com/example/springproj5/FirebaseTest.java
@@ -1,59 +1,68 @@
 package com.example.springproj5;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.JSONException;
-import org.json.JSONObject;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.AuthenticationException;
-
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.Collections;
+import org.json.JSONObject;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 
 public class FirebaseTest {
 
-    TestRestTemplate restTemplate = new TestRestTemplate();
+    @Test
+    public void test() throws IOException, InterruptedException {
+        // Login and get authorization token
+        String responseBody = authenticate();
+        JSONObject resBody = new JSONObject(responseBody);
+        String idToken = resBody.getString("idToken");
+        String userId = resBody.getString("localId");
 
-    HttpHeaders headers = new HttpHeaders();
+        // Upload image to Firebase storage and get file reference out of response
+        String fileReference = uploadImage(idToken, userId);
 
-    public String authenticate() throws AuthenticationException, NullPointerException {
+        // Set avatar property in user document
+        setAvatar(idToken, fileReference, userId);
+
+        // Get image reference from avatar property
+        String userDetailsResult = getUserDetails(userId, idToken);
+        String avatarReference = new JSONObject(userDetailsResult).getJSONObject("fields").getJSONObject("avatar").getString("stringValue");
+        String avatarReferenceUrlEncoded = URLEncoder.encode(avatarReference, StandardCharsets.UTF_8.toString());
+
+        // Check if file with this reference exists in storage
+        String url = String.format("%s/%s?alt=media&token=%s", ApiUrlConstants.FIREBASE_STORAGE_AVATAR, avatarReferenceUrlEncoded, System.getenv("FIREBASE_TOKEN"));
+        assertEquals(200, HTTPUtil.check(url, idToken));
+    }
+
+    public String getUserDetails(String userId, String idToken) throws IOException {
+        String url = String.format("%s/%s", ApiUrlConstants.FIRESTORE_DOCUMENT, userId);
+        return HTTPUtil.get(url, idToken);
+    }
+
+    public void setAvatar(String idToken, String fileReference, String userId) throws IOException, InterruptedException {
+        String url = String.format("%s/%s?updateMask.fieldPaths=avatar", ApiUrlConstants.FIRESTORE_DOCUMENT, userId);
+        JSONObject data = new JSONObject();
+        JSONObject fieldsObject = new JSONObject();
+        JSONObject avatarObject = new JSONObject();
+        avatarObject.put("stringValue", fileReference);
+        fieldsObject.put("avatar", avatarObject);
+        data.put("fields", fieldsObject);
+
+        HTTPUtil.patch(url, idToken, data.toString());
+    }
+
+    public String uploadImage(String idToken, String userId) throws IOException, InterruptedException {
+        String url = String.format("%s/%s%%2F%s?alt=media&token=%s", ApiUrlConstants.FIREBASE_STORAGE_AVATAR, userId, "avatar.jpg", System.getenv("FIREBASE_TOKEN"));
+        String response = HTTPUtil.uploadFile(url, idToken, "avatar.jpg");
+        return new JSONObject(response).getString("name");
+    }
+
+    public String authenticate() throws IOException, InterruptedException {
         JSONObject data = new JSONObject();
         data.put("email", System.getenv("FIREBASE_EMAIL"));
         data.put("password", System.getenv("FIREBASE_PASSWORD"));
-        data.put("returnSecureToken", true);
-        HttpEntity<String> entity = new HttpEntity<>(data.toString(), headers);
-
-        ResponseEntity<String> response = restTemplate.exchange(
-                ApiUrlConstants.FIREBASE_SIGNIN_EMAIL_AND_PASSWORD.concat(System.getenv("FIREBASE_API_KEY")), HttpMethod.POST, entity, String.class);
-        return response.getBody();
-    }
-
-    public String getUserDetails(String idToken, String userId) {
-        headers.put("Authorization", Collections.singletonList("Bearer ".concat(idToken)));
-        HttpEntity<String> entity = new HttpEntity<>(null, headers);
-
-        ResponseEntity<String> response = restTemplate.exchange(
-                ApiUrlConstants.FIRESTORE_USERS.concat(userId), HttpMethod.GET, entity, String.class);
-        return response.getBody();
-    }
-
-    @Test
-    public void endToEndTest() throws JSONException {
-        String responseBody = authenticate();
-        JSONObject resBody = new JSONObject(responseBody);
-        String idToken = (String) resBody.get("idToken");
-        String userId = (String) resBody.get("localId");
-        String userDetails = new JSONObject(getUserDetails(idToken, userId)).get("fields").toString();
-        try {
-            User user = new ObjectMapper().readValue(userDetails, User.class);
-            assertTrue(true);
-        } catch (Exception e) {
-            fail();
-        }
+        data.put("returnSecureToken", "true");
+        return HTTPUtil.post(ApiUrlConstants.FIREBASE_SIGNIN_EMAIL_AND_PASSWORD.concat(System.getenv("FIREBASE_API_KEY")), data);
     }
 }
